83e42f52c8d276a96465bf31f3173201fe9a33b9
Simon Thörnqvist <simon@plug-n-trade.com>
2018-02-08 12:41:48 +0100

    add push event module

---
+++ apps/azra_receiver/lib/azra_receiver/hook/push_event.ex
@@ -0,0 +1,24 @@
+defmodule AzraReceiver.Hook.PushEvent do
+  defstruct id: nil,
+    timestamp: nil,
+    action: nil,
+    target: %{},
+    request: %{}
+
+  use ExConstructor
+
+  def as_docker_hub(%__MODULE__{request: request, target: target} = event) do
+    push_data = Map.new |> Map.put("tag", target["tag"])
+    repository = Map.new |> Map.put("repo_name", full_name(event))
+
+    %{"push_data" => push_data, "repository" => repository}
+  end
+
+
+  def pretty_print(%__MODULE__{timestamp: timestamp, action: action, target: target, request: request} = event) do
+    "#{timestamp} :: Action: #{String.upcase(action)} -> #{full_name(event)}}\nUser-Agent :: #{request["useragent"]}\nType :: #{target["mediaType"]}\nDigest :: #{target["digest"]} -> #{target["size"]}"
+  end
+
+  defp full_name(%__MODULE__{request: request, target: target}),
+    do: "#{request["host"]}/#{target["repository"]}"
+end
