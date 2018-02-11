2918c41067b02ceb33447df433ad8ec86383b689
Simon Thörnqvist <simon@plug-n-trade.com>
2018-02-11 22:03:53 +0100

    use local server conf, start server immediately

--- apps/azra_server/mix.exs
+++ apps/azra_server/mix.exs
@@ -6,7 +6,7 @@ defmodule AzraServer.Mixfile do
       app: :azra_server,
       version: "0.1.0",
       build_path: "../../_build",
-      config_path: "../../config/config.exs",
+      config_path: "./config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.6",
