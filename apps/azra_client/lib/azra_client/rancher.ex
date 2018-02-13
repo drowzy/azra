5b6f66b4ee201d4da4accaea0e649e3067dad67c
Simon Thörnqvist <simon@plug-n-trade.com>
2018-02-13 09:00:16 +0100

    http client

--- apps/azra_client/lib/azra_client/rancher.ex
+++ apps/azra_client/lib/azra_client/rancher.ex
@@ -1,1 +1,23 @@
 
+defmodule AzraClient.Rancher do
+  defmodule Receiver do
+    defstruct id: nil, tag: nil, driver: nil, url: nil, selectors: []
+
+    def new(data) do
+      tag = Kernel.get_in(data, ["serviceUpgradeConfig", "tag"])
+      selectors =
+        data
+        |> Kernel.get_in(["serviceUpgradeConfig", "serviceSelector"])
+        |> Enum.into([])
+        |> Enum.map(fn {_label, value} -> value end)
+
+      %__MODULE__{
+        id: data["id"],
+        driver: data["driver"],
+        url: data["url"],
+        tag: tag,
+        selectors: selectors,
+      }
+    end
+  end
+end
