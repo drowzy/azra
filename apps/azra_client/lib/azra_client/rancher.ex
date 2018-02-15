941ff146a3fce88cd662a9f5692b8ef639c78c14
Simon Thörnqvist <simon@plug-n-trade.com>
2018-02-15 22:31:09 +0100

    remove old definition

--- apps/azra_client/lib/azra_client/rancher.ex
+++ apps/azra_client/lib/azra_client/rancher.ex
@@ -2,34 +2,6 @@ defmodule AzraClient.Rancher do
   alias AzraClient.Rancher.{Client, Receiver}
   require Logger
 
-  defmodule Receiver do
-    defstruct id: nil, tag: nil, driver: nil, url: nil, selectors: []
-
-    def new(data) do
-      tag = Kernel.get_in(data, ["serviceUpgradeConfig", "tag"])
-
-      selectors =
-        data
-        |> Kernel.get_in(["serviceUpgradeConfig", "serviceSelector"])
-        |> parse_selector
-        |> Enum.into([])
-
-      %__MODULE__{
-        id: data["id"],
-        driver: data["driver"],
-        url: data["url"],
-        tag: tag,
-        selectors: selectors
-      }
-    end
-
-    def match?(%__MODULE__{selectors: selectors}, selector),
-      do: Enum.any?(selectors, &(&1 == selector))
-
-    defp parse_selector(nil), do: %{}
-    defp parse_selector(selector), do: selector
-  end
-
   use GenServer
 
   def start_link(opts) do
